b9e084963a5c0c9a83f458af7b18ee3ddc71adf7
Yuyang Zhong <yuyang.zhong.2125@gmail.com>
2019-04-15 18:05:06 -0700

    completed hw8

--- hw/hw8/assets/scripts/q1.js
+++ hw/hw8/assets/scripts/q1.js
@@ -1,6 +1,7 @@
 
 //
-// Part 1: Code setBackgroundColor() so the background color of #q1-container changes to the color specified
+// Part 1: Code setBackgroundColor() so the background color of #q1-container 
+// changes to the color specified
 //
 
 /**
@@ -9,38 +10,36 @@
  * @param {string} color The background color that #q1-container should change to
  */
 function setBackgroundColor(color) {
-    // TODO: Get the element with the id "q1-container" and set its background color to the input color
-
-
-
-
-
+    // TODO: Get the element with the id "q1-container" and set its background 
+    // color to the input color
+    document.getElementById("q1-container").style.background = color;
 }
 
 //
-// Part 2: Code the function redButtonClicked() and make the browser run the function for you when #q1-btn-red is clicked
+// Part 2: Code the function redButtonClicked() and make the browser run the 
+// function for you when #q1-btn-red is clicked
 //
 
 // For the red button
 // Use the color #EF476F, or change it up to your liking
-// If you do decide to change the color, probably also consider updating the corresponding button color in CSS :)
+// If you do decide to change the color, probably also consider updating the 
+// corresponding button color in CSS :)
 
 /**
  * Handler function that the browser should run when the red button is clicked
  */
 function redButtonClicked() {
-    // TODO: Use the setBackgroundColor() to set the background color to #EF476F (or another color of your choice)
-
-
-
-
-
+    // TODO: Use the setBackgroundColor() to set the background color to #EF476F 
+    // (or another color of your choice)
+    setBackgroundColor("#EF476F");
 }
 
-// This gets the element with id "q1-btn-red" and sets the browser to run the function redButtonClicked() when someone clicks the element
+// This gets the element with id "q1-btn-red" and sets the browser to run the 
+// function redButtonClicked() when someone clicks the element
 document.getElementById("q1-btn-red").onclick = redButtonClicked;
 
-// After you finish part 1 & 2, the clicking the red button should turn the background color red!
+// After you finish part 1 & 2, the clicking the red button should turn the 
+// background color red!
 
 //
 // Part 3: Program the yellow button similar to what you did with the red button
@@ -50,16 +49,13 @@ document.getElementById("q1-btn-red").onclick = redButtonClicked;
 // Use the color #FFD166, or change it up to your liking
 
 // TODO: Define a function like yellowButtonClicked() that sets the background color
+function yellowButtonClicked() {
+	setBackgroundColor("#FFD166")
+}
 
-
-
-
-
-// TODO: Set the browser to run the function yellowButtonClicked() when someone clicks #q1-btn-yellow
-
-
-
-
+// TODO: Set the browser to run the function yellowButtonClicked() when someone 
+// clicks #q1-btn-yellow
+document.getElementById("q1-btn-yellow").onclick = yellowButtonClicked;
 
 //
 // Part 4: A little more practice, let's do the same for the rest of the buttons
@@ -69,21 +65,30 @@ document.getElementById("q1-btn-red").onclick = redButtonClicked;
 // Use the color #06D6A0, or change it up to your liking
 
 // TODO: Make the green button work similarly
+function greenButtonClicked() {
+	setBackgroundColor("#06D6A0")
+}
 
-
-
-
+document.getElementById("q1-btn-green").onclick = greenButtonClicked;
 
 // For the light blue button
 // Use the color #118AB2, or change it up to your liking
 
 // TODO: Make the light blue button work similarly
 
+function lightBlueButtonClicked() {
+	setBackgroundColor("#118AB2")
+}
 
-
-
+document.getElementById("q1-btn-lightblue").onclick = lightBlueButtonClicked;
 
 // For the blue button
 // Use the color #073B4C, or change it up to your liking
 
 // TODO: Make the blue button work similarly
+
+function blueButtonClicked() {
+	setBackgroundColor("#073B4C")
+}
+
+document.getElementById("q1-btn-blue").onclick = blueButtonClicked;
--- hw/hw8/assets/scripts/q2.js
+++ hw/hw8/assets/scripts/q2.js
@@ -1,9 +1,11 @@
 // 
-// Part 1: Fill in the function unhideLightbox. Afterwards, we will make this function run when a picture is clicked on.
+// Part 1: Fill in the function unhideLightbox. Afterwards, we will make this 
+// function run when a picture is clicked on.
 // 
 
 /**
- * Makes lightbox overlay visible, and shows the lightbox pop-up corresponding to the id
+ * Makes lightbox overlay visible, and shows the lightbox pop-up corresponding 
+ * to the id
  *
  * @param {string} lightboxID The id of the lightbox pop-up to show
  */
@@ -12,9 +14,8 @@ function unhideLightbox(lightboxID) {
 	document.getElementById('lightbox-overlay').classList.remove('hidden');
 
 	// TODO: Remove the .hidden class from the div with the given id
-
+	document.getElementById(lightboxID).classList.remove('hidden');
 }
-
 
 //
 // Part 2: Make the unhideLightbox function run when a picture is clicked on.
@@ -24,44 +25,52 @@ function unhideLightbox(lightboxID) {
  * Calls unhideLightbox with the id of the first lightbox
  */
 function unhideLightbox1() {
-	// TODO: Look in q2.html to see what the id for the lightbox div for the first picture, and call unhideLightbox
-
+	// TODO: Look in q2.html to see what the id for the lightbox div for the 
+	// first picture, and call unhideLightbox
+	unhideLightbox("doggo1");
 }
 
-// TODO: Set the browser to run the function unhideLightbox1 when someone clicks #picture-1
-
+// TODO: Set the browser to run the function unhideLightbox1 when someone clicks 
+// #picture-1
+document.getElementById("picture-1").onclick = unhideLightbox1;
 
 // TODO: do the same for #picture-2, write a function and then make it run on click
+function unhideLightbox2() {
+	unhideLightbox("doggo2");
+}
 
-
-
-
+document.getElementById("picture-2").onclick = unhideLightbox2;
 
 // TODO: do the same for #picture-3, write a function and then make it run on click
 
+function unhideLightbox3() {
+	unhideLightbox("doggo3");
+}
 
-
-
-
+document.getElementById("picture-3").onclick = unhideLightbox3;
 
 // 
-// Part 3: Now we need to be able to close the lightbox when we click outside the picture!
+// Part 3: Now we need to be able to close the lightbox when we click outside the 
+// picture!
 // 
 function closeLightbox(lightboxID) {
 	// This adds the .hidden class to the #lightbox-overlay div
 	document.getElementById('lightbox-overlay').classList.add('hidden');
 
 	// TODO: Add the .hidden class to the div with the given id
+	document.getElementById(lightboxID).classList.add('hidden');
 
 }
 
 
-// This function class closeLightbox() for every .lightbox div - feel free to read it, but you do not have to understand it completely!
+// This function class closeLightbox() for every .lightbox div - feel free to read 
+// it, but you do not have to understand it completely!
 function closeAllLightboxes() {
 	// get every .lightbox div, getElementsByClassName gives us an array 
 	var lightboxElements = document.getElementsByClassName('lightbox');
 
-	// sneak preview of Javascript loops, which will go through every element in an array of elements
+	// sneak preview of Javascript loops, which will go through every element in 
+	// an array of elements
 	for (var i = 0; i < lightboxElements.length; i++) {
 		// get id of this particular .lightbox div
 		var id = lightboxElements[i].id;
@@ -70,4 +79,6 @@ function closeAllLightboxes() {
 	}
 }
 
-// TODO: set the closeAllLightboxes function to run when #lightbox-overlay is clicked on.
+// TODO: set the closeAllLightboxes function to run when #lightbox-overlay is 
+// clicked on.
+document.getElementById('lightbox-overlay').onclick = closeAllLightboxes;
