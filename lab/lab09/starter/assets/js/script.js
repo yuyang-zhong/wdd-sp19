63e9c8c34824db51558fa0ea4e69ef8cc45160ba
Yuyang Zhong <yuyang.zhong.2125@gmail.com>
2019-04-19 12:30:17 -0700

    completed lab09

--- lab/lab09/starter/assets/js/script.js
+++ lab/lab09/starter/assets/js/script.js
@@ -1,9 +1,25 @@
 $(document).ready(function() {
 	const distanceToNextImage = -450;
 	let currentImageNumber = 0;
+	$("#lightbox").hide();
 
 	// YOUR CODE HERE
+	$("#right").click(function(){
+		currentImageNumber += 1;
+		$("#carousel-strip").css("left", currentImageNumber * distanceToNextImage);
+	})
 
+	$("#left").click(function(){
+		currentImageNumber -= 1;
+		$("#carousel-strip").css("left", currentImageNumber * distanceToNextImage);
+	})
+
+
+	$(".item").click(function(){
+		currentImageNumber = parseInt($(this).attr('id'));
+		$("#carousel-strip").css("left", currentImageNumber * distanceToNextImage);
+		$("#lightbox").show();
+	})
 	
 	// OTHER CODE
 	// This closes the lightbox when you click on the overlay or the x.
