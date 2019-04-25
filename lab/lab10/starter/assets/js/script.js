fb0be89f18b2a919cf4788175466eb0b28348866
Yuyang Zhong <yuyang.zhong.2125@gmail.com>
2019-04-24 19:59:17 -0700

    completed lab10

--- lab/lab10/starter/assets/js/script.js
+++ lab/lab10/starter/assets/js/script.js
@@ -2,6 +2,21 @@ $(document).ready(function()  {
 
 	// PART 1: YOUR CODE HERE
 
+	for (let i = 1; i <= 6; i += 1) {
+		$("#content").append(
+			'<div class="fruit"><img src="assets/img/fruit.jpeg"></div>');
+	}
+
+	for (let i = 1; i <= 12; i += 1) {
+		$("#content").append(
+			'<div class="water"><img src="assets/img/water.jpeg"></div>');
+	}
+
+	for (let i = 1; i <= 8; i += 1) {
+		$("#content").append(
+			'<div class="bread"><img src="assets/img/bread.jpeg"></div>');
+	}
+
 	$(".nav-item").click(function() {
 
 		let buttonName = $(this).text();
@@ -9,24 +24,17 @@ $(document).ready(function()  {
 
 		// UNCOMMENT THE SECTION BELOW FOR PART 2
 		
-		/*
-		
 		// YOUR CODE HERE - PART 2.1
 
-		if (condition) {
+		if (!buttonActive) {
 			$(this).addClass("active");
 			$(this).removeClass("inactive");
-
-			// YOUR CODE HERE - PART 2.2
+			$("." + buttonName).show();
 
 		} else {
 			$(this).addClass("inactive");
 			$(this).removeClass("active");
-
-			// YOUR CODE HERE - PART 2.2
-
+			$("." + buttonName).hide();
 		} 
-		*/
-		
 	})
 })
